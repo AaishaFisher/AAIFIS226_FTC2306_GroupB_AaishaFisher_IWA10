a2670bb830a15fa25a01a5380f857b12a0d67e13
Aai'sha Fisher <aaishafisher484@gmail.com>
2023-08-25 13:30:11 +0200

    challenge 1 complete

--- Challenge1.js
+++ Challenge1.js
@@ -68,7 +68,7 @@ console.log(message);
 const copied = { ...holidays[christmas]}; //shallow copies object without altering stored values
 copied.name = 'X-mas Day';
 const correctDate = new Date(copied.date);  //targets christmas date 
-correctDate.setHours(0,0,0,0);
+correctDate.setHours(0,0,0,0); //resets the time
 correctDate.setMinutes(0);
 
 const isEarlier = correctDate.getTime() < holidays[6].date.getTime();
@@ -78,41 +78,81 @@ if (isEarlier) {
     copied.date = correctDate;
 };
 
-console.log('ID change:', holidays[christmas].id != copied.id || copied.id)
-console.log('Name change:', holidays[christmas].name != copied.name || copied.name)
-console.log('Date change:', holidays[christmas].date != copied.date || copied.date)
+console.log('ID change:', holidays[christmas].id !== copied.id ? copied.id : false); // false
+console.log('Name change:', holidays[christmas].name !== copied.name ? copied.name : false); //true, will log xmas
+console.log('Date change:', holidays[christmas].date.getTime() !== correctDate.getTime() ? correctDate : false); //true, so will log 26/12 12 am
+
+
+
 
 const firstHolidayTimestamp = Math.min(
-    holidays[0].date.getTime,
-    holidays[1].date.getTime,
-    holidays[2].date.getTime,
-    holidays[3].date.getTime,
-    holidays[4].date.getTime,
-    holidays[5].date.getTime,
-    holidays[6].date.getTime,
-    holidays[7].date.getTime,
-    holidays[8].date.getTime,
+    holidays[0].date.getTime(),
+    holidays[1].date.getTime(),
+    holidays[2].date.getTime(),
+    holidays[3].date.getTime(),
+    holidays[4].date.getTime(),
+    holidays[5].date.getTime(),
+    holidays[6].date.getTime(),
+    holidays[7].date.getTime(),
+    holidays[8].date.getTime(),
 )
 
 const lastHolidayTimestamp = Math.max(
-    holidays[0].date.getTime,
-    holidays[1].date.getTime,
-    holidays[2].date.getTime,
-    holidays[3].date.getTime,
-    holidays[4].date.getTime,
-    holidays[5].date.getTime,
-    holidays[6].date.getTime,
-    holidays[7].date.getTime,
-    holidays[8].date.getTime,
+    holidays[0].date.getTime(),
+    holidays[1].date.getTime(),
+    holidays[2].date.getTime(),
+    holidays[3].date.getTime(),
+    holidays[4].date.getTime(),
+    holidays[5].date.getTime(),
+    holidays[6].date.getTime(),
+    holidays[7].date.getTime(),
+    holidays[8].date.getTime(),
 )
 
-const firstDay = firstHolidayTimestamp.getDate
-const firstMonth = firstHolidayTimestamp.getMonth
-const lastDay = lastHolidayTimestamp.getDate
-const lastMonth = lastHolidayTimestamp.getMonth
+//
 
-console.log(`${firstDay}/${firstMonth}/${currentYear}`)
-console.log(`${lastDay}/${lastMonth}/${currentYear}`)
+// Utility function to convert date strings to Date objects
+function convertToDateObject(dateString) {
+    if (typeof dateString === 'string') {
+        return new Date(dateString);
+    }
+    return dateString;
+}
 
-const randomHoliday = holidays[Math.random]
-console.log(randomHoliday.date)
+for (let i = 0; i <= 8; i++) {
+    holidays[i].date = convertToDateObject(holidays[i].date);
+}
+
+
+//
+
+//declared our dates
+let firstHoliday = holidays[3].date; // New Year's Day is the first holiday
+let lastHoliday = holidays[2].date;  // Day of Goodwill is the last holiday in the year
+
+// Getting the day, month, and year of the first and last holidays
+const firstDay = String(firstHoliday.getDate()).padStart(2, '0');
+const firstMonth = String(firstHoliday.getMonth() + 1).padStart(2, '0');
+const firstYear = currentYear;
+const lastDay = String(lastHoliday.getDate()).padStart(2, '0');
+const lastMonth = String(lastHoliday.getMonth() + 1).padStart(2, '0');
+const lastYear = currentYear;
+
+// Formatting and displaying the first and last holiday dates
+console.log(`${firstDay}/${firstMonth}/${currentYear}`);
+console.log(`${lastDay}/${lastMonth}/${currentYear}`);
+
+// Selecting a random holiday
+const randomHolidayIndex = Math.floor(Math.random() * 9); // 9 holidays in total
+const randomHolidayDate = new Date(holidays[randomHolidayIndex].date);
+
+// Getting the day and month of the random holiday
+const randomHolidayDay = String(randomHolidayDate.getDate()).padStart(2, '0');
+const randomHolidayMonth = String(randomHolidayDate.getMonth() + 1).padStart(2, '0');
+
+// Formatting and displaying the random holiday date
+console.log(`${randomHolidayDay}/${randomHolidayMonth}/${currentYear}`);
+
+
+//will throw an error: Uncaught TypeError: holiday.date.getTime is not a function
+
